62848176ce45a438e884c6566b5ccaef7e990b7e
Guilherme Faria Miarelli <guilherme-fm@hotmail.com>
2014-05-23 22:45:08 -0300

    UsuarioBeanV2

--- src/main/java/br/com/recomusic/bean/UsuarioBean.java
+++ src/main/java/br/com/recomusic/bean/UsuarioBean.java
@@ -1,6 +1,7 @@
 package br.com.recomusic.bean;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -11,63 +12,91 @@ import br.com.recomusic.om.Usuario;
 import br.com.recomusic.singleton.ConectaBanco;
 
 
- 
 @ManagedBean(name="UsuarioBean")
 @ViewScoped
 public class UsuarioBean implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 	private UsuarioDAO usuarioDAO;
-	private String email;
+	private long pkUsuario;
+	private String emailUsuario;
+	private String login;
 	private String senha;
-	
+	private String nome;
+	private String sobrenome;
+	private Date lancamento;
+	private Date dataNascimento;
+	private int sexo;
 	private boolean logado;
-	
+
 	public UsuarioBean()
 	{
-        usuarioDAO = new UsuarioDAO( ConectaBanco.getInstance().getEntityManager());
-        logado = false;
-    }
-	
+		usuarioDAO = new UsuarioDAO( ConectaBanco.getInstance().getEntityManager());
+		logado = false;
+	}
+
 	public void logar()
 	{
-        try
-        {
-        	boolean validacaoUsuario = usuarioDAO.validarUsuario(email, senha);
-			this.logado = validacaoUsuario;
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-            ConectaBanco.getInstance().rollBack();
-        }
+		try
+		{
+			Usuario usuario = usuarioDAO.validarUsuario(emailUsuario, senha);
+			if(usuario!=null && usuario.getPkUsuario()>0)
+			{
+				this.logado = true;
+			}
+			else
+			{
+				this.logado = false;
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			ConectaBanco.getInstance().rollBack();
+		}
 	}
 	
+	public void salvar()
+	{
+		try
+		{
+			if(emailUsuario.contains("@"))
+			{
+				this.logado = true;
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			ConectaBanco.getInstance().rollBack();
+		}
+	}
+
 	public void deslogar()
 	{
 		this.logado = false;
 	}
-	
-	 public void save(Usuario usuario)
-	 {
-	        try
-	        {
-	        	ConectaBanco.getInstance().beginTransaction();
-	            usuarioDAO.save(usuario);
-	            ConectaBanco.getInstance().commit();
-	        }
-	        catch(Exception e)
-	        {
-	            e.printStackTrace();
-	            ConectaBanco.getInstance().rollBack();
-	        }
-	    }
-	     
-	    public List<Usuario> findAll()
-	    {
-	        return usuarioDAO.findAll();
-	    }
-	
+
+	public void save(Usuario usuario)
+	{
+		try
+		{
+			ConectaBanco.getInstance().beginTransaction();
+			usuarioDAO.save(usuario);
+			ConectaBanco.getInstance().commit();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			ConectaBanco.getInstance().rollBack();
+		}
+	}
+
+	public List<Usuario> findAll()
+	{
+		return usuarioDAO.findAll();
+	}
+
 	// 		Getters and Setters			//
 	
 	public boolean getLogado() {
@@ -78,12 +107,20 @@ public class UsuarioBean implements Serializable
 		this.logado = logado;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getNome() {
+		return nome;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmailUsuario() {
+		return emailUsuario;
+	}
+
+	public void setEmailUsuario(String emailUsuario) {
+		this.emailUsuario = emailUsuario;
 	}
 
 	public String getSenha() {
@@ -94,5 +131,51 @@ public class UsuarioBean implements Serializable
 		this.senha = senha;
 	}
 
-	
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public long getPkUsuario() {
+		return pkUsuario;
+	}
+
+	public void setPkUsuario(long pkUsuario) {
+		this.pkUsuario = pkUsuario;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public Date getLancamento() {
+		return lancamento;
+	}
+
+	public void setLancamento(Date lancamento) {
+		this.lancamento = lancamento;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public int getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(int sexo) {
+		this.sexo = sexo;
+	}
 }
