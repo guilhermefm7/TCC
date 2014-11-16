a9321688b2b3cafe733655a8a41dd1c9461b7363
Guilherme Faria Miarelli <guilherme@hdntecnologia.com.br>
2014-11-16 14:33:23 -0200

    Dialog Cadastro

--- src/main/webapp/resources/js/pesquisaMusica.js
+++ src/main/webapp/resources/js/pesquisaMusica.js
@@ -29,7 +29,25 @@ function redirecionarPagina()
 
 function centerAndShowDialog(dialog)
 {
-    $(dialog).css("top",Math.max(0,(($(window).height() - $(dialog).outerHeight()) / 2) + $(window).scrollTop()) + "px");
-    $(dialog).css("left",Math.max(0, (($(window).width() - $(dialog).outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+    if(($(window).width())<=600)
+    {
+    	$(dialog).css("top",Math.max(0,(($(window).height() - $(dialog).outerHeight()) / 1.5) ) + "px");
+    	$(dialog).css("left",Math.max(0, (($(window).width() - $(dialog).outerWidth()) / 5) ) + "px");
+    	$(dialog).css("width",Math.max(0, (($(window).width() ) / 1.5) ) + "px");
+    	$(dialog).css("height",Math.max(0, (($(window).height() ) / 1.25) ) + "px");
+    }
     PF('dialogCadastro1').show();
+}
+
+function centerAndShowDialog1()
+{
+	var dialog  = document.getElementById('dialogCadastro1');
+	if(($(window).width())<=600)
+	{
+		$(dialog).css("top",Math.max(0,(($(window).height() - $(dialog).outerHeight()) / 1.5) ) + "px");
+		$(dialog).css("left",Math.max(0, (($(window).width() - $(dialog).outerWidth()) / 5) ) + "px");
+		$(dialog).css("width",Math.max(0, (($(window).width() ) / 1.5) ) + "px");
+		$(dialog).css("height",Math.max(0, (($(window).height() ) / 1.25) ) + "px");
+	}
+	PF('dialogCadastro1').show();
 }
